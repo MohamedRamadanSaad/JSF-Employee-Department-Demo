2826bf00dfbb7ea473f5c83f5062e1dba9b85c60
Mohamed Ramadan <m.ramadansaad@gmail.com>
2020-01-29 20:21:09 +0200

    Update HibernateUtil.java

--- src/java/com/hibernate/service/HibernateUtil.java
+++ src/java/com/hibernate/service/HibernateUtil.java
@@ -14,7 +14,7 @@ import org.hibernate.cfg.Configuration;
  * Hibernate Utility class with a convenient method to get Session Factory
  * object.
  *
- * @author dirac
+ * @author Mohamed Ramadan
  */
 public class HibernateUtil {
     
